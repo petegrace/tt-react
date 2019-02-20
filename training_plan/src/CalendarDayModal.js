2de33f07a735c03805083643d16751ee046e95af
petegrace <petergrace84@gmail.com>
2019-02-20 21:09:57 +0000

    display activities and support remove from state

--- training_plan/src/CalendarDayModal.js
+++ training_plan/src/CalendarDayModal.js
@@ -3,18 +3,24 @@ import dateFns from "date-fns";
 
 import "./CalendarDayModal.css";
 import PlannedActivitiesList from "./PlannedActivitiesList"
+//import PlannedActivityForm from "./PlannedActivityForm";
 
 const CalendarDayModal = (props) => {
     const dateFormat = "dddd DD MMMM YYYY";
+    const calendarDay = props.calendarDay;
+
     return (
         <div className="modal-back-drop">
             <div className="modal-wrapper">
                 <div className="calendar-modal-header">
                     <h4>{dateFns.format(props.calendarDay, dateFormat)}</h4>
-                    <span className="close-modal-btn" onClick={props.close}><i class="fa fa-window-close"></i></span>
+                    <span className="close-modal-btn" onClick={props.close}><i className="fa fa-window-close"></i></span>
                 </div>
                 <div className="calendar-modal-body">
-                    <PlannedActivitiesList calendarDay={props.calendarDay} />
+                        <div>
+                            <PlannedActivitiesList calendarDay={calendarDay} />
+                            {/* TODO: toggle visibility for <PlannedActivityForm /> */}
+                        </div>
                     {props.children}
                 </div>
             </div>
--- training_plan/src/PlannedActivitiesList.js
+++ training_plan/src/PlannedActivitiesList.js
@@ -14,7 +14,6 @@ class PlannedActivitiesList extends Component {
     componentDidMount() {
         const accessToken = ls.get("accessToken");
         const authHeader = "Bearer " + accessToken;
-
         const options = {
             method: "GET",
             headers: {
@@ -23,7 +22,8 @@ class PlannedActivitiesList extends Component {
         };
         const endpointOrigin = window.location.origin === "http://localhost:3000" ? "http://localhost:5000" : (window.location.origin);
         const dateFormat = "YYYY-MM-DD";
-        const endpoint = endpointOrigin + "/api/planned_activities/" + dateFns.format(this.props.calendarDay, dateFormat);
+        const endpoint = endpointOrigin + "/api/planned_activities?startDate=" + dateFns.format(this.props.calendarDay, dateFormat);
+        console.log(endpoint);
         fetch(endpoint, options).then(r => {
             r.json().then(response => {
                 this.setState(response);
@@ -31,9 +31,74 @@ class PlannedActivitiesList extends Component {
         });
     }
 
-    render() {
+    handleRemoveActivity = (key) => {
+        let filteredActivities = this.state.planned_activities.filter(function(plannedActivity) {
+            return plannedActivity.id != key;
+        });
+
+        this.setState({
+            "planned_activities": filteredActivities
+        })
+
+        // const authHeader = "Bearer " + accessToken;
+        // const options = {
+        //     method: "GET",
+        //     headers: {
+        //         "Authorization": authHeader
+        //     }
+        // };
+        // const endpointOrigin = window.location.origin === "http://localhost:3000" ? "http://localhost:5000" : (window.location.origin);
+        // const dateFormat = "YYYY-MM-DD";
+        // const endpoint = endpointOrigin + "/api/planned_activities?startDate=" + dateFns.format(this.props.calendarDay, dateFormat);
+        // console.log(endpoint);
+        // fetch(endpoint, options).then(r => {
+        //     r.json().then(response => {
+        //         this.setState(response);
+        //     });
+        // });
+    }
+
+    createPlannedActivityRow = (plannedActivity) => {
+        const badgeClass = "badge badge-primary " +  plannedActivity.category_key;
+
         return (
-            <p>Activity 1 - Social run - 10k - One-off - Invite others</p>
+            <tr key={plannedActivity.id}>
+                <td><h4><span className={badgeClass}>{plannedActivity.activity_type}</span></h4></td>
+                <td>{plannedActivity.planned_distance}</td>
+                <td>{plannedActivity.description}</td>
+                <td>Repeats every {dateFns.format(this.props.calendarDay, "dddd")}</td>
+                <td>
+                    <ul className="nav justify-content-end">
+                        <li className="nav-item mr-5">
+                            {/* TODO: link to toggle visibility: <i className="fa fa-edit"></i> Edit</NavLink> */}
+                        </li>
+                        <li className="nav-item mr-5">
+                            <a href="#" role="button" onClick={() => this.handleRemoveActivity(plannedActivity.id)}><i className="fa fa-trash"></i> Remove</a>
+                        </li>
+                    </ul>
+                </td>
+            </tr>
+        );
+    }
+
+    render() {
+        let plannedActivities = this.state.planned_activities;
+        let plannedActivityRows = plannedActivities.map(this.createPlannedActivityRow);
+
+        return (
+                <div>
+                    {plannedActivities.length > 0 && (
+                        <>
+                        <h3>Planned Activities</h3>
+                        <table className="table table-sm table-hover mt-3">
+                            <tbody>
+                            {plannedActivityRows}
+                            </tbody>
+                        </table>
+                        </>
+                    )}
+                    <p>Activity 1 - Social run - 10k - One-off - Invite others</p>
+                </div>
         );
     }
 }
